44211e5fdb79057419b87df8708414dfabafee9a
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-05-09 16:55:39 +0700

    using zustand first time

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,9 +1,25 @@
-import Image from "next/image";
+"use client";
+import useCount from "./store/Count";
 
 export default function Home() {
+  const { count, increment, decrement } = useCount();
   return (
-    <main>
-      <div>HELLO</div>
+    <main className="h-screen flex justify-center">
+      <div className="flex gap-4 items-center">
+        <button
+          onClick={increment}
+          className="border-solid border-2 border-black px-6 bg-black text-white rounded-lg text-[20px] font-semibold"
+        >
+          +
+        </button>
+        <span className="font-bold text-[25px]">{count}</span>
+        <button
+          onClick={decrement}
+          className="border-solid border-2 border-black px-6 bg-black text-white rounded-lg text-[20px] font-semibold"
+        >
+          -
+        </button>
+      </div>
     </main>
   );
 }
